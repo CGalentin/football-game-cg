d1528ca48fe48ec0ee07b9e13d051c486efc6ba5
Chris Galentinemail <chrisgalentin.bloomtech@gmail.com>
2025-10-28 19:07:33 -0400

    Day 4 committ

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -4,3 +4,20 @@ interface ScoreboardProps {
     score: Score,
     possession: Team;
 }
+export default function Scoreboard ({score, possession}:ScoreboardProps) {
+    return (
+        <section className= 'bg-white rounded-lg shadow p-4 w-full max-w-xl'>
+            <h2 className="text-xl font-bold text-gray-800 mb-2">Scoreboard</h2>
+
+            <div className="flex justify-between text-lg font-semibold text-grey-700">
+                <span>Home: {score.HOME}</span>
+                <span>Away: {score.AWAY}</span>
+            </div>
+
+            <p className="mt-2 text-sm text-gray-600">
+                Possession:{""}
+                <span className="font-bold text-indigo-600">Possession</span>            
+            </p>
+        </section>
+    )
+}
